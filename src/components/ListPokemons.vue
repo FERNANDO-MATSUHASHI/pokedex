0464c5823531345342b2ac343f040163fa2ec83d
vani <kodimavani@gmail.com>
2023-09-05 22:30:17 -0300

    Add Image

---
+++ src/components/ListPokemons.vue
@@ -0,0 +1,25 @@
+<script setup>
+const pokemon = defineProps(["name", "url"])
+
+</script>
+
+<template>
+    <div class="col-4">
+        <div class="card mb-3">
+            <!-- <p class="text-center">{{ pokemon.name }}</p> -->
+            <div class="card" style="width: 18rem;">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                <h5 class="card-title">{{ pokemon.name }}</h5>
+                <p class="card-text"></p>
+                <a href="#" class="btn btn-primary">Informações</a>
+            </div>
+</div>
+        </div>
+    </div>
+
+</template>
+
+<style>
+
+</style>
